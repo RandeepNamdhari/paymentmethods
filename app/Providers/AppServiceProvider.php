fc9fcff9046f5d1b7c63b2ed3e2453a065a631dc
root <randeepnamdhari87@gmail.com>
2022-01-22 20:03:26 +0530

    provide changed for https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (!App::environment([
+        if (!\App::environment([
             'local',
             'testing',
         ])) {
